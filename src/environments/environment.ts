587859dd11c312d539dcfd2a86021af0adc3e0e0
AngelEvaristo <62860453+AngelEvaristo@users.noreply.github.com>
2025-09-06 11:37:06 -0500

    prueba token gh

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:3000',
-  githubToken: "ErSyL4mdy1M2ELcQSFoS8CW1nkZXyp0KBaeN"
+  githubToken: "TjsqjKt5BTXpTp7G8VddhsiEamcmGz4Pgx3o"
 };
